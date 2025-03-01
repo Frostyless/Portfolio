af1c9cf4ab1f1b5ff2ae773735e82d4c4dc4546c
Frostyless <ivanlamyuxian@gmail.com>
2025-03-02 00:54:06 +0800

    Hello

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -19,12 +19,12 @@ const Services = () => {
        )
    })
   return (
-    <div id="services"className=' flex flex-col bg-black min-h-screen p-10 text-white'> 
-     <div className="max-w-4xl mx-auto mt-5 ">
-        <div className=' p-6 flex w-full items-center justify-center md:justify-start lg:justify-start  '>
-            <h1 ref={serviceRef} className='text-7xl font-semi-bold mb-10 tracking-wide font-plex-mono'>Services</h1>
-        </div>
-        <div className="flex flex-col md:flex-row gap-12 w-full items-center justify-center ">
+    <div id="services" className="flex flex-col bg-black min-h-screen p-10 text-white">
+    <div className="w-full max-w-4xl mx-auto mt-5 px-4">
+      <div className="p-6 flex w-full items-center justify-center md:justify-start lg:justify-start">
+        <h1 className="text-7xl font-semibold mb-10 tracking-wide font-plex-mono">Services</h1>
+      </div>
+      <div className="flex flex-col md:flex-row gap-12 w-full items-center justify-center">
         <ServiceCard
           price="$200+"
           title="Brand Identity & Logo Design"
@@ -59,7 +59,7 @@ function ServiceCard ({price, title, description, items}) {
         )
     })
     return (
-        <div ref={(el) => cardRef.current.push(el)}  className=' border border-white/50 bg-gray-100 p-12 rounded-2xl w-full min-w-sm md:max-w-md lg:max-w-lgborder border-gray-white relative '>
+        <div ref={(el) => cardRef.current.push(el)}  className=' border border-white/50 bg-gray-100 p-12 rounded-2xl w-full min-w-sm md:max-w-md lg:max-w-lg relative '>
             <div className='text-sm border border-white/50 px-2 py-1 rounded-md inline-block'>
                 {price}
             </div>
@@ -79,7 +79,7 @@ function ServiceCard ({price, title, description, items}) {
                             </li>
                 })}
             </ul>
-            <button className='mt-15 w-full bg-#1E1E1D hover:bg-black text-white py-2 rounded-full border border-white'>
+            <button className='mt-16 w-full bg-[#1E1E1D] hover:bg-black text-white py-2 rounded-full border border-white'>
                 SHOW +
             </button>
         </div>
